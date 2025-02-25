2bd522ab9e1e076f4961190e1893c565fbba3e87
Sumit Singh <sumitsingh3076@gmail.com>
2025-02-25 14:19:57 +0530

    chat..

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -8,31 +8,32 @@ export default function Chat() {
 
   return (
     <div className="min-h-screen flex flex-col bg-black text-white">
-      <div className="flex justify-end pt-8 pr-12">
-      <UserButton />
+
+      <div className="flex justify-between pt-8 pr-12">
+        <h1 className="text-xl font-semibold pl-6 font-mono">ChatUr-Bot</h1>
+        <UserButton />
       </div>
       {/* Chat Messages */}
       <div className="flex-1 overflow-y-auto p-4 space-y-3 flex flex-col mr-56">
-  {messages.map((m) => (
-    <div
-      key={m.id}
-      className={`flex w-full ${m.role === "user" ? "justify-end" : "justify-start"}`}
-    >
-      <div
-        className={`px-4 py-2 rounded-2xl w-fit max-w-[75%] break-words whitespace-pre-wrap ${
-          m.role === "user"
-            ? "bg-[#1d1f1d] text-right"
-            : "bg-[#2a2d2a] text-left"
-        }`}
-      >
-        <span className="block font-medium text-gray-400 text-left">
-          {m.role === "user" ? "You" : "AI"}
-        </span>
-        <span className="block text-white">{m.content}</span>
+        {messages.map((m) => (
+          <div
+            key={m.id}
+            className={`flex w-full ${m.role === "user" ? "justify-end" : "justify-start"}`}
+          >
+            <div
+              className={`px-4 py-2 rounded-2xl w-fit max-w-[75%] break-words whitespace-pre-wrap ${m.role === "user"
+                ? "bg-[#1d1f1d] text-right"
+                : "bg-[#2a2d2a] text-left"
+                }`}
+            >
+              <span className="block font-medium text-gray-400 text-left">
+                {m.role === "user" ? "You" : "AI"}
+              </span>
+              <span className="block text-white">{m.content}</span>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
-  ))}
-</div>
 
 
       {/* Chat Input */}
@@ -46,9 +47,8 @@ export default function Chat() {
         <button
           type="submit"
           disabled={!input.trim()}
-          className={`p-3 rounded-full ${
-            input.trim() ? "bg-[#1d1f1d] hover:bg-[#2c2d2c]" : "bg-[#1d1f1d]"
-          } transition-colors`}
+          className={`p-3 rounded-full ${input.trim() ? "bg-[#1d1f1d] hover:bg-[#2c2d2c]" : "bg-[#1d1f1d]"
+            } transition-colors`}
         >
           <FiSend size={20} className="text-white" />
         </button>
