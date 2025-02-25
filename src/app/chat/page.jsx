2a1c1c5a3e4f0f2d67d55b6488cc9ea1625a8dff
Ashmit111 <ashmitsingh862@gmail.com>
2025-02-25 16:51:45 +0530

    Style Update

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -21,9 +21,9 @@ export default function Chat() {
             className={`flex w-full ${m.role === "user" ? "justify-end" : "justify-start"}`}
           >
             <div
-              className={`px-4 py-2 rounded-2xl w-fit max-w-[75%] break-words whitespace-pre-wrap ${m.role === "user"
+              className={`px-4 py-2 rounded-2xl w-fit max-w-[55%] break-words whitespace-pre-wrap ${m.role === "user"
                 ? "bg-[#1d1f1d] text-right"
-                : "bg-[#2a2d2a] text-left"
+                : "bg-[#2a2d2a] text-left ml-52 mt-8 mb-6"
                 }`}
             >
               <span className="block font-medium text-gray-400 text-left">
